6ef0f2e01c6799d9a54faf8a5b12d7f9c706bad9
jkos95 <78613540+jkos95@users.noreply.github.com>
2024-01-16 15:06:47 -0500

    Update Undocking-Script.py
    Updated code. Changed to PyQt from Tkinter. Code is from tutorial.

--- Undocking-Script.py
+++ Undocking-Script.py
@@ -1,7 +1,9 @@
 # IMPORTS
 
-# tk is GUI presentation and control
-import tkinter as tk
+import sys
+
+#modules needed to create a GUI. QtWidgets contains all major widgets
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QMessageBox
 
 # Declare arrays to store info
 winID = []
@@ -12,10 +14,41 @@ winWidth = []
 
 # MAIN
 
-window = tk.Tk()
+def dialog():
+    mbox = QMessageBox()
 
-greeting = tk.Label(text="Hello world!")
+    mbox.setText("Your allegiance has been noted")
+    mbox.setDetailedText("You are now a disciple and subject of the all-knowing Guru")
+    mbox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
 
-# required by Tkinter. Tells it to run the main event loop.
-# Tk will NOT run without this line
-window.mainloop()
+    mbox.exec_()
+
+if __name__ == "__main__":
+
+    # create object of QApplication class. All GUIs must create an instance of the QApplication as an entry to the app.
+    app = QApplication(sys.argv)
+
+    # create object of the QWidget class and add modifiers below.
+    w = QWidget()
+    w.resize(350, 200)
+    w.setWindowTitle("Undocking Script")
+
+    # creates label for object w and assigns to "label"
+    label = QLabel(w)
+    label.setText("Behold the Guru, Guru99")
+    label.move(100,130)
+    label.show()
+
+    # creates button for object w and assigns to "btn"
+    btn = QPushButton(w)
+    btn.setText('Beheld')
+    btn.move(110,150)
+    btn.show()
+    btn.clicked.connect(dialog)
+
+
+    # displays widget by object name
+    w.show()
+
+    # app.exec_() starts the Qt/C++ event loop. This passes execution from Python over to Qt, and Python will wait until the GUI is closed to proceed.
+    sys.exit(app.exec_())
